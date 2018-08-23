2142243d15dbc204de3663e29f7c4605414ad023
lipracer <13137721573@163.com>
2018-08-23 20:57:35 +0800

    update code

---
+++ huffmanEncode/huffman/heap.hpp
@@ -0,0 +1,15 @@
+#ifndef  _HEAP_H_
+#define _HEAP_H_
+
+#define parent(n) ((n-1)/2)
+#define lchild(n) ((2*n)+1)
+#define rchild(n) ((2*n)+2)
+
+template<typename T>
+int make_heap(T* array, size_t len) 
+{
+
+}
+
+#endif // ! _HEAP_H_
+
