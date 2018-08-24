ee53c029304c00caaa88e6b5c0f5cdf14ab58bc3
chenlonglong <chenlonglong@qiyi.com>
2018-08-24 13:42:06 +0800

    add code

--- huffmanEncode/huffman/heap.hpp
+++ huffmanEncode/huffman/heap.hpp
@@ -1,15 +1,88 @@
-#ifndef  _HEAP_H_
-#define _HEAP_H_
+#ifndef _HEAP_HPP_
 
 #define parent(n) ((n-1)/2)
-#define lchild(n) ((2*n)+1)
-#define rchild(n) ((2*n)+2)
+#define lchrd(n) ((2*n)+1)
+#define rchrd(n) ((2*n)+2)
 
-template<typename T>
-int make_heap(T* array, size_t len) 
+enum  CmpType : int
+{
+    less,
+};
+
+template<typename T, CmpType cmpType>
+class LLHeap
 {
 
-}
+    bool compare1(T lt, T rt) 
+    {
+        return lt < rt;
+    }
+    bool compare2(T lt, T rt)
+    {
+        return lt >= rt;
+    }
+    
+public:
+    LLHeap(T* src, size_t len):_src(src), _len(len)
+    {
+        make_heap();
+    }
+    int(*_cmp)(T, T);
+    LLHeap() 
+    {
+        if (cmpType)
+        {
+            _cmp = compare2;
+        }
+        else 
+        {
+            _cmp = compare1;
+        }
+    }
+    inline int adjust(T* src, size_t loc)
+    {
+        while (true)
+        {
+            size_t nParent = parent(loc);
+            if (_cmp(src[loc], src[nParent]))
+            {
+                //½»»»
+                loc = nParent;
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
 
-#endif // ! _HEAP_H_
+    int make_heap()
+    {
+        if (_len < 2)
+        {
+            return 0;
+        }
+        else
+        {
+            for (size_t i = 2; i < _len; i++)
+            {
+                adjust(_src, i);
+            }
+        }
+        return 0;
+    }
+
+    int pop_back() 
+    {
+
+    }
+private:
+    T* _src;
+    size_t _len;
+};
+
+
+
+#endif // !_HEAP_H_
 
